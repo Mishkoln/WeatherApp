56faad3c6fea13697decac6736cffb53bd12a643
Mishkoln <shkolnik.mi@gmail.com>
2019-05-01 00:50:10 +0300

    Server with JS, client with cache, npm

---
+++ start.js
@@ -0,0 +1,6 @@
+const server = require('./staticServer.js');
+const hostname = '127.0.0.1';
+const port = 8080;
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
